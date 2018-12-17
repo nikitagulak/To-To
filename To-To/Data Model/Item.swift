09f9c2acd0da1d04353d1f2a1c3e5eb3cae157e8
Nikita <myepicland@gmail.com>
2018-12-17 21:17:30 +0100

    Convert to MVC design pattern

---
+++ To-To/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  To-To
+//
+//  Created by Nick on 17/12/2018.
+//  Copyright © 2018 Yofa. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
